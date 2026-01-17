1b8f75148c445b34af0a7dd884b389cb3ec11598
PiQuark6046 <piquark6046@proton.me>
2026-01-17 17:00:09 +0000

    test: create simple test units for `HTTPS2Request` func

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { HTTPSRequest } from '@/index.js'
+import { HTTPSRequest, HTTPS2Request } from '@/index.js'
 
 test('www.example.com HTML request', async T => {
   let Url = new URL('https://www.example.com/')
@@ -11,6 +11,20 @@ test('www.example.com HTML request', async T => {
 test('JSON request without ExpectedAs', async T => {
   let Url = new URL('https://api64.ipify.org?format=json')
   let HTTPSRes = await HTTPSRequest(Url)
+  T.is(HTTPSRes.StatusCode, 200)
+  T.true(typeof HTTPSRes.Body === 'object' && HTTPSRes.Body instanceof ArrayBuffer)
+})
+
+test('HTTP/2 www.example.com HTML request', async T => {
+  let Url = new URL('https://www.example.com/')
+  let HTTPSRes = await HTTPS2Request(Url, { ExpectedAs: 'String' })
+  T.is(HTTPSRes.StatusCode, 200)
+  T.true(typeof HTTPSRes.Body === 'string')
+})
+
+test('HTTP/2 JSON request without ExpectedAs', async T => {
+  let Url = new URL('https://api64.ipify.org?format=json')
+  let HTTPSRes = await HTTPS2Request(Url)
   T.is(HTTPSRes.StatusCode, 200)
   T.true(typeof HTTPSRes.Body === 'object' && HTTPSRes.Body instanceof ArrayBuffer)
 })
